1b7ef1b14f87d5e722dbbc6b6458fd2b9dc642e3
CharlotteAlbouy <albouy.charlotte@gmail.com>
2024-10-15 12:32:03 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+import chalk from "chalk";
+
+const sayHello = () => {
+	console.log(chalk.green("Reumie"));
+	console.log(chalk.red("Tomeuh"));
+	console.log(chalk.blue("Abedoueuh"));
+	console.log(chalk.yellow("Syleuvia"));
+};
+
+sayHello();
